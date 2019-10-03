8010bd0fff9f154e2a076b938268e5093885f9fc
DidinSalahudin <didinsalahudin@gmail.com>
2019-10-03 16:55:02 +0700

    Updates

---
+++ precache-manifest.9ca2abe5091af536b27447555452eeb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d880864eba71709b4859c789e64111",
+    "url": "/alquran/index.html"
+  },
+  {
+    "revision": "e5602f08ad5e1f242c78",
+    "url": "/alquran/static/css/main.d06e4fa8.chunk.css"
+  },
+  {
+    "revision": "3c56c954439d9f26f973",
+    "url": "/alquran/static/js/2.2d5f7474.chunk.js"
+  },
+  {
+    "revision": "e5602f08ad5e1f242c78",
+    "url": "/alquran/static/js/main.6affe0f7.chunk.js"
+  },
+  {
+    "revision": "025ea215b32a41f5dacf",
+    "url": "/alquran/static/js/runtime~main.b1638580.js"
+  },
+  {
+    "revision": "557336d4deab6dcb67e3bd72f34fa92e",
+    "url": "/alquran/static/media/loader.557336d4.gif"
+  }
+]);
